57b9c7de1ab966fd37c227b4ae04764270795b96
dheekshakoka <dheeksha.koka@gmail.com>
2026-08-02 21:12:46 +0200

    study time calculation

---
+++ m1/l1/project_1.py
@@ -0,0 +1,10 @@
+print('\n study time calculation🙂\n')
+wake_up_time = input("What time do you wake up? ")
+print("Your wake-up time is", wake_up_time)
+if wake_up_time == "6:00 AM" or wake_up_time == "6:30 AM" or wake_up_time == "7:00 AM":
+    print("You have enough time to study before school.")
+else:
+    print("You might not have enough time for studying before school.")
+print("going to school")
+print("playing")
+print("studying")
